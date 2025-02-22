61404711754b0d4fad8d9be075765a79ba404ef0
Andrew <andrewklad@yandex.ru>
2025-02-22 18:16:35 +0300

    Add solution for task; create main class of library

---
+++ TestConnector/TestConnector.cs
@@ -0,0 +1,47 @@
+﻿using ConnectorTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHQ;
+
+namespace TestConnector
+{
+    public class TestConnector : ITestConnector
+    {
+        public event Action<Trade> NewBuyTrade;
+        public event Action<Trade> NewSellTrade;
+        public event Action<Candle> CandleSeriesProcessing;
+
+        public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeCandles(string pair, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeTrades(string pair, int maxCount = 100)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnsubscribeCandles(string pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnsubscribeTrades(string pair)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
