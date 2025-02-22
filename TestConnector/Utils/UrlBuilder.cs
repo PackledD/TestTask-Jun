c55ae0026b7ff6d8047012be82563f0d7295729a
Andrew <andrewklad@yandex.ru>
2025-02-23 00:17:06 +0300

    Add class for build url from domain and params

---
+++ TestConnector/Utils/UrlBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConnector.Utils
+{
+    internal static class UrlBuilder
+    {
+        public static string Build(string urlBase, IEnumerable<string> urlParams)
+        {
+            string res = urlBase;
+            foreach (var param in urlParams)
+            {
+                res += $"{param}&";
+            }
+            return res.TrimEnd('&');
+        }
+    }
+}
