d0c70355c11effaecf5dc1b765ea1089f77759c9
Andrew <andrewklad@yandex.ru>
2025-02-23 00:17:57 +0300

    Add interfaces for REST API interactions

---
+++ TestConnector/Rest/Interface/ICandleSeriesFetcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHQ;
+
+namespace TestConnector.Rest.Interface
+{
+    internal interface ICandleSeriesFetcher
+    {
+        Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to, long? count);
+    }
+}
---
+++ TestConnector/Rest/Interface/INewTradesFetcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHQ;
+
+namespace TestConnector.Rest.Interface
+{
+    internal interface INewTradesFetcher
+    {
+        Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount);
+    }
+}
